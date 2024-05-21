fac59a7b59adac70d06b05549257452bd9df0f79
Mohit Kumar <mkr9395@gmail.com>
2024-05-21 16:46:29 +0530

    gguf not working

---
+++ src/run_local.py
@@ -0,0 +1,39 @@
+from langchain import PromptTemplate
+from langchain import LLMChain
+from langchain.llms import CTransformers
+from src.helper import *
+
+B_INST, E_INST = "[INST]","[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+
+instruction = "Convert the following text from English to Hindi: \n\n {text}"
+
+SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+
+template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+
+
+# creating the prompt :
+prompt  = PromptTemplate(
+    template = template,
+    input_variables = ['text']
+)
+
+# Loading the LLM Model
+
+llm = CTransformers(
+    model='model/llama-2-7b-chat.Q4_0.gguf',
+    model_type = 'llama',
+    config = {'max_new_tokens':128,
+              'temperature':0.01} 
+)
+
+
+# creating the LLM Chain
+
+LLM_Chain = LLMCHAIN(
+    prompt = prompt,
+    llm=llm
+)
+
+print(LLM_Chain.invoke("How are you?"))
